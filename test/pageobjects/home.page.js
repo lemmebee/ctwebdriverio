9ad9fa4439f7642d9c205d4cbd570da5e0102ec0
EhabShaaban <ehabsfeki@gmail.com>
2021-07-25 16:34:47 +0200

    Add home page

---
+++ test/pageobjects/home.page.js
@@ -0,0 +1,16 @@
+const Page = require('./page');
+
+class HomePage extends Page {
+
+    get inputSearch () { return $('#search') }
+
+    async search (searchText) {
+        await (await this.inputSearch).setValue(searchText);
+    }
+
+    open () {
+        return super.open('');
+    }
+}
+
+module.exports = new HomePage();
