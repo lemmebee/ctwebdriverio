76c6047b35f8501c75762a5689057c9ed6c2518a
EhabShaaban <ehabsfeki@gmail.com>
2021-07-27 10:31:40 +0200

    Add allure configs

--- wdio.conf.js
+++ wdio.conf.js
@@ -135,7 +135,14 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [
+        [
+            'allure',
+            {
+                outputDir: 'allure-results',
+            },
+        ],
+    ],
 
 
     
