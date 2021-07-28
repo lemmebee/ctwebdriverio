6bc74d1505eceb8ab0eae0f205599d2d9ea48662
EhabShaaban <ehabsfeki@gmail.com>
2021-07-28 19:19:46 +0200

    Uncomment login tests

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -5,28 +5,28 @@ import AccountDashboard from '../pageobjects/account-dashboard.page'
 
 describe('Login test suite', () => {
 
-    // it('should not login with invalid credentials', async () => {
+    it('should not login with invalid credentials', async () => {
         
-    //     await LoginPage.open();
-    //     await LoginPage.login('test@test.com', 'ThisIs@T3st');
-    //     await expect(LoginPage.spanInvalidLoginErrorMessage).toBeExisting();
-    //     await expect(LoginPage.spanInvalidLoginErrorMessage).toHaveTextContaining(
-    //         'Invalid login or password.');
+        await LoginPage.open();
+        await LoginPage.login('test@test.com', 'ThisIs@T3st');
+        await expect(LoginPage.spanInvalidLoginErrorMessage).toBeExisting();
+        await expect(LoginPage.spanInvalidLoginErrorMessage).toHaveTextContaining(
+            'Invalid login or password.');
 
-    // });
+    });
 
-    // it('should not login with empty credentials', async () => {
+    it('should not login with empty credentials', async () => {
 
-    //     await LoginPage.open();
-    //     await LoginPage.login('', '');
-    //     await expect(LoginPage.divEmptyEmailValidation).toBeExisting();
-    //     await expect(LoginPage.divEmptyEmailValidation).toHaveTextContaining(
-    //         'This is a required field.');
-    //     await expect(LoginPage.divEmptyPasswordValidation).toBeExisting();
-    //     await expect(LoginPage.divEmptyPasswordValidation).toHaveTextContaining(
-    //         'This is a required field.');
+        await LoginPage.open();
+        await LoginPage.login('', '');
+        await expect(LoginPage.divEmptyEmailValidation).toBeExisting();
+        await expect(LoginPage.divEmptyEmailValidation).toHaveTextContaining(
+            'This is a required field.');
+        await expect(LoginPage.divEmptyPasswordValidation).toBeExisting();
+        await expect(LoginPage.divEmptyPasswordValidation).toHaveTextContaining(
+            'This is a required field.');
 
-    // });
+    });
 
     it('should login with valid credentials', async () => {
 
