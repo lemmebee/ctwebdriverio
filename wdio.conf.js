1bec99df1384bab451ebc03f4d4cb59e81d8ced9
EhabShaaban <ehabsfeki@gmail.com>
2021-07-28 08:09:34 +0200

    Update browser capabilities

--- wdio.conf.js
+++ wdio.conf.js
@@ -60,7 +60,12 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
-        acceptInsecureCerts: true
+        acceptInsecureCerts: true,
+        'goog:chromeOptions': {
+            args: [
+                '--window-size=1920,1080'
+                ],
+        }
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
