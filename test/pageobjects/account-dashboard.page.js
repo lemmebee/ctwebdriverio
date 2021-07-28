82c73cbb2a36c431e53017570b18a7aab634c047
EhabShaaban <ehabsfeki@gmail.com>
2021-07-28 19:18:55 +0200

    Add login with valid creds test

--- test/pageobjects/account-dashboard.page.js
+++ test/pageobjects/account-dashboard.page.js
@@ -5,6 +5,8 @@ let successMessage = "Thank you for registering with Madison Island."
 class AccountDashboard extends Page {
 
     get spanRegisterSuccessMessage () { return $(`span*=${successMessage}`) }
+    get h1DashboardLabel () { return $('div[class="page-title"] h1') }
+    get strongGreetings () { return $('p[class="hello"] strong') }
 
     open () {
         return super.open('customer/account/index/');
--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,27 +1,42 @@
 import LoginPage from '../pageobjects/login.page'
+import * as loginCreds from '../json-helper/registered-user.json'
+import AccountDashboard from '../pageobjects/account-dashboard.page'
+
 
 describe('Login test suite', () => {
 
-    it('should not login with invalid credentials', async () => {
+    // it('should not login with invalid credentials', async () => {
         
+    //     await LoginPage.open();
+    //     await LoginPage.login('test@test.com', 'ThisIs@T3st');
+    //     await expect(LoginPage.spanInvalidLoginErrorMessage).toBeExisting();
+    //     await expect(LoginPage.spanInvalidLoginErrorMessage).toHaveTextContaining(
+    //         'Invalid login or password.');
+
+    // });
+
+    // it('should not login with empty credentials', async () => {
+
+    //     await LoginPage.open();
+    //     await LoginPage.login('', '');
+    //     await expect(LoginPage.divEmptyEmailValidation).toBeExisting();
+    //     await expect(LoginPage.divEmptyEmailValidation).toHaveTextContaining(
+    //         'This is a required field.');
+    //     await expect(LoginPage.divEmptyPasswordValidation).toBeExisting();
+    //     await expect(LoginPage.divEmptyPasswordValidation).toHaveTextContaining(
+    //         'This is a required field.');
+
+    // });
+
+    it('should login with valid credentials', async () => {
+
         await LoginPage.open();
-        await LoginPage.login('test@test.com', 'ThisIs@T3st');
-        await expect(LoginPage.spanInvalidLoginErrorMessage).toBeExisting();
-        await expect(LoginPage.spanInvalidLoginErrorMessage).toHaveTextContaining(
-            'Invalid login or password.');
+        await LoginPage.login(loginCreds.user.email, loginCreds.user.password);
+        await expect(AccountDashboard.h1DashboardLabel).toBeExisting();
+        await expect(AccountDashboard.h1DashboardLabel).toHaveTextContaining('MY DASHBOARD');
 
-    });
-
-    it('should not login with empty credentials', async () => {
-
-        await LoginPage.open();
-        await LoginPage.login('', '');
-        await expect(LoginPage.divEmptyEmailValidation).toBeExisting();
-        await expect(LoginPage.divEmptyEmailValidation).toHaveTextContaining(
-            'This is a required field.');
-        await expect(LoginPage.divEmptyPasswordValidation).toBeExisting();
-        await expect(LoginPage.divEmptyPasswordValidation).toHaveTextContaining(
-            'This is a required field.');
+        await expect(AccountDashboard.strongGreetings).toBeExisting();
+        await expect(AccountDashboard.strongGreetings).toHaveTextContaining('Hello, '+loginCreds.user.firstName+' '+loginCreds.user.lastName+'!');
 
     });
 });
