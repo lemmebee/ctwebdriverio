18da6b426c27053363cac9d12127eb67449dd2d3
EhabShaaban <ehabsfeki@gmail.com>
2021-07-28 19:39:45 +0200

    Add logout test

--- test/pageobjects/account-dashboard.page.js
+++ test/pageobjects/account-dashboard.page.js
@@ -1,15 +1,23 @@
 import Page from './page'
 
 let successMessage = "Thank you for registering with Madison Island."
+let accountLabel = "Account"
 
 class AccountDashboard extends Page {
 
     get spanRegisterSuccessMessage () { return $(`span*=${successMessage}`) }
     get h1DashboardLabel () { return $('div[class="page-title"] h1') }
     get strongGreetings () { return $('p[class="hello"] strong') }
+    get spanAccountLabel () { return $(`span*=${accountLabel}`) }
+    get aLogoutLabel () { return $('a[title="Log Out"]') }
 
     open () {
         return super.open('customer/account/index/');
+    }
+
+    async logout () {
+        await (await this.spanAccountLabel).click();
+        await (await this.aLogoutLabel).click();
     }
 }
 
---
+++ test/specs/logout.e2e.js
@@ -0,0 +1,16 @@
+import LoginPage from '../pageobjects/login.page'
+import * as loginCreds from '../json-helper/registered-user.json'
+import AccountDashboard from '../pageobjects/account-dashboard.page'
+
+
+describe('Logout test suite', () => {
+
+    it('should logout successfully', async () => {
+
+        await LoginPage.open();
+        await LoginPage.login(loginCreds.user.email, loginCreds.user.password);
+        await expect(AccountDashboard.h1DashboardLabel).toHaveTextContaining('MY DASHBOARD');
+        await AccountDashboard.logout();
+        await expect(browser).toHaveUrl('http://www.ctqatest.biz/ecom/customer/account/logoutSuccess/');
+    });
+});
