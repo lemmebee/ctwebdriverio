1a4e76047c5ad51035f36d61413cbcde5bb21926
EhabShaaban <ehabsfeki@gmail.com>
2021-09-14 17:47:33 +0200

    Add password

--- test/specs/logout.e2e.js
+++ test/specs/logout.e2e.js
@@ -3,6 +3,9 @@ import * as loginCreds from '../json-helper/registered-user.json'
 import AccountDashboard from '../pageobjects/account-dashboard.page'
 import allureReporter from '@wdio/allure-reporter'
 
+
+let password = "nbdf151_adaf45_afdf456";
+
 /**
  * This is a test file containing tests for the following
  * behaviour on ctqatest.biz/ecom/: Logout feature
@@ -18,6 +21,8 @@ describe('Logout test suite', () =>
         await expect(AccountDashboard.h1DashboardLabel).toHaveTextContaining('MY DASHBOARD');
         await AccountDashboard.logout();
         await expect(browser).toHaveUrl('http://www.ctqatest.biz/ecom/customer/account/logoutSuccess/');
+
+        cosnst password = "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";
         
     });
 });
